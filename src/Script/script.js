bd5ae5868814e670f9ad5d537035234eb26836b9
Alentejo <arthur.alentejo228@gmail.com>
2022-03-27 22:23:35 -0300

    Finzalizando efeito do header

--- src/Script/script.js
+++ src/Script/script.js
@@ -1,8 +1,27 @@
-window.onscroll = () => {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    console.log("olaaaaaaa");
-    document.querySelector("#header").style.height = "10vh";
-  } else {
-    document.querySelector("#header").style.height = "13vh";
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+const header = document.querySelector("#header")
+window.addEventListener("scroll", () => {
+  //shrink header
+  if(scrollY > 10){
+    header.classList.add("small-header")
+  } else{
+    header.classList.remove("small-header")
   }
-}
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    console.log(`Element: ${section.id} \nScrollY: ${scrollY} \n ${sectionTop - sectionHeight / 3}` );
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("destacado");
+    if (li.classList.contains(current)) {
+      li.classList.add("destacado");
+    }
+  });
+});
