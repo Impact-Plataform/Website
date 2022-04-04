cb461515d9e1395821d4557292fe794934277e38
Alentejo <arthur.alentejo228@gmail.com>
2022-04-04 15:01:52 -0300

    Testing

--- src/Script/carousel.js
+++ src/Script/carousel.js
@@ -1,20 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.items').slick({
-        infinite: true,
+        dots: true,
+        infinite: false,
+        speed: 300, 
         autoplay: true,
-        autoplaySpeed: 1800,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [{
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                autoplaySpeed: 1800,
-                speed: 300
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
             }
-        }]
-    });
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 });
