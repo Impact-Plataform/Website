6fb966826d8c270ce3179245b5075447f1f296b8
Alentejo <arthur.alentejo228@gmail.com>
2022-04-04 22:51:03 -0300

    finishing faq section

--- src/Script/script.js
+++ src/Script/script.js
@@ -42,3 +42,15 @@ document.querySelectorAll(".nav ul li").forEach((li) => {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+
+const details = Array.from(document.querySelectorAll('details'))
+  
+details.forEach((detail) => {
+  detail.addEventListener('click', (e) => {
+    const active = details.find(d => d.open)
+    if (!e.currentTarget.open && active) {
+      active.open = false
+    }
+  })
+})
