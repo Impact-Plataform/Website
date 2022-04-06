ba9895c92aadd933548f7fabba87acf6c4d40c0a
Hesfra <contato.chfreire@gmail.com>
2022-04-06 17:13:31 -0300

    criando  Carrosel para a aba de depoimentos

--- src/Script/carousel.js
+++ src/Script/carousel.js
@@ -18,30 +18,10 @@ $(document).ready(function() {
       }]
   });
 
-  
-$('.dep').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+  $('.dep').slick({
+    autoplay: true,
+      autoplaySpeed: 15000,
+      speed: 2000,
+  });
+
 });
